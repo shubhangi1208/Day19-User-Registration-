04d7037c155b3c95bced9cc1862817e1b5bb8836
shubhangi1208 <ashubhangi1996@gmail.com>
2022-01-23 20:03:20 +0530

    ADDED:User can Validate Through Last Name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,5 +1,4 @@
 package com.bridgelabz;
-import java.security.PublicKey;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,5 +17,17 @@ public class UserRegistration {
             System.out.println("First name is Valid");
         else
             System.out.println("First name is Invalid");
+    }
+    public static void validLastName(){
+        System.out.print("Enter the Last Name : ");
+        String name = scanner.next();
+        String regex = "^[A-Z]{1}[a-z]{3,}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(name);
+        boolean r = m.matches();
+        if (r)
+            System.out.println("Last Name is Valid");
+        else
+            System.out.println("Last Name is Invalid");
     }
 }
--- src/com/bridgelabz/UserRegistrationMain.java
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -1,7 +1,5 @@
 package com.bridgelabz;
-
 import java.util.Scanner;
-
 public class UserRegistrationMain {
 
     public static void main(String[] args) {
@@ -11,17 +9,20 @@ public class UserRegistrationMain {
         int choice;
 
         do {
-            System.out.println(" 1.To validate First Name \n 2. Quit ");
+            System.out.println(" 1.To validate First Name \n 2.To validate Last Name \n 3.Quit ");
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
                     UserRegistration.validFirstName();
                     break;
                 case 2:
+                    UserRegistration.validLastName();
+                    break;
+                case 3:
                     System.out.println("Thank You ");
                     break;
             }
 
-        } while (choice != 2);
+        } while (choice != 3);
     }
 }
